abcc1f2945dd1fdc0db65ed92daf6154440071d7
lukemew <123739531+lukemew@users.noreply.github.com>
2023-11-19 12:58:08 -0300

    Name changing

--- Benchmark_busca&ordenacao/appBusca.h
+++
@@ -1,8 +0,0 @@
-#ifndef APPBUSCA_H
-#define APPBUSCA_H
-
-int buscarLinear(int elemento, int numeros[], int tamanho);
-int buscarSentinela(int elemento, int numeros[], int tamanho);
-int buscarBinaria(int elemento, int numeros[], int tamanho);
-
-#endif 
